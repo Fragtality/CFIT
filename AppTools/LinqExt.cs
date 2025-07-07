1b84e293d54cbcb9abdd653ff886707664fea100
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-07-07 19:07:10 +0200

    AppTools LinqExt

---
+++ AppTools/LinqExt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CFIT.AppTools
+{
+    public static partial class Enumerable
+    {
+        public static TSource SafeLast<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null || source?.Count() == 0)
+                return (TSource)(object)null;
+
+            return source.Last();
+        }
+
+        public static TSource SafeLast<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null || source?.Count() == 0)
+                return (TSource)(object)null;
+
+            return source.Last(predicate);
+        }
+    }
+}

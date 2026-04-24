ea146333d7fa615c2ea5471763d391efceeef864
Fragtality <78614902+Fragtality@users.noreply.github.com>
2026-04-24 21:12:58 +0200

    AppFramework: - added SafeEnumConverter

---
+++ AppFramework/UI/ValueConverter/SafeEnumConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CFIT.AppFramework.UI.ValueConverter
+{
+    public class SafeEnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                return value?.ToString() ?? "";
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str && targetType.IsEnum)
+            {
+                try
+                {
+                    return Enum.Parse(targetType, str);
+                }
+                catch
+                {
+                    return Binding.DoNothing;
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+    }
+}
